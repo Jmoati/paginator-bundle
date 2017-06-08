05b2fa5fb90d943644a65966bc397f8b97186536
Jacques Moati <jacques@moati.net>
2017-06-08 22:14:43 +0200

    First version

---
+++ src/JmoatiPaginatorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jmoati\PaginatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JmoatiPaginatorBundle extends Bundle
+{
+}
---
+++ src/Service/PaginatorService.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Jmoati\PaginatorBundle\Service;
+
+use Doctrine\ORM\QueryBuilder;
+
+class PaginatorService
+{
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @return int
+     */
+    protected function getCount(QueryBuilder $queryBuilder): int
+    {
+        $qb = clone $queryBuilder;
+        $qb->select('COUNT(DISTINCT '.$qb->getRootAliases()[0].'.id)');
+
+        return (int)$qb->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param int $offset
+     * @param int $limit
+     * @return array
+     */
+    protected function getIds(QueryBuilder $queryBuilder, int $offset, int $limit): array
+    {
+        $qb = clone $queryBuilder;
+        $qb
+            ->select('DISTINCT '.$qb->getRootAliases()[0].'.id')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        return array_column($qb->getQuery()->getResult(), 'id');
+    }
+    
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param array $ids
+     * @return array
+     */
+    public function getItems(QueryBuilder $queryBuilder, array $ids): array
+    {
+        $qb = clone $queryBuilder;
+
+        $qb
+            ->andWhere($qb->getRootAliases()[0].'.id IN (:ids)')
+            ->setParameter('ids', $ids);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param int $page
+     * @param int $limit
+     * @return array
+     */
+    public function paginate(QueryBuilder $queryBuilder, int $page = 1, int $limit = 10): array
+    {
+        $offset = ($page-1) * $limit;
+
+        return [
+            'total' => $this->getCount($queryBuilder),
+            'items' => $this->getItems($queryBuilder, $this->getIds($queryBuilder, $offset, $limit)),
+        ];
+    }
+}
